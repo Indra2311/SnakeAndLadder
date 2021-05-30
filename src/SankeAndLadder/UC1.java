bc9091bfe1ec444d90eba747a047053a744682a1
Indrajeet <indrajeetseru@gmail.com>
2021-05-30 18:33:35 +0530

    Initial Commit

---
+++ src/SankeAndLadder/UC1.java
@@ -0,0 +1,9 @@
+package SankeAndLadder;
+
+public class UC1 {
+
+public static void main(String[] args) {
+		
+		System.out.println("Start Position is Zero");
+	}
+}
---
+++ src/SankeAndLadder/UC2.java
@@ -0,0 +1,11 @@
+package SankeAndLadder;
+
+public class UC2 {
+
+	public static void main(String[] args) {
+		int die = (int) (Math.random() * 6) + 1;
+		System.out.println("The Dice Number is "+die+".");
+
+	}
+
+}
